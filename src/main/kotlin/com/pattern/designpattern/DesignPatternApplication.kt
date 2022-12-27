489e0b199e13ef286797cc5c1769d1e06e39643b
tungtranthai <tranthaitung@traveloka.com>
2022-12-27 13:14:45 +0700

    Init

---
+++ src/main/kotlin/com/pattern/designpattern/DesignPatternApplication.kt
@@ -0,0 +1,11 @@
+package com.pattern.designpattern
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesignPatternApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesignPatternApplication>(*args)
+}
